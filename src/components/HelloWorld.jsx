9fd11fc6387b067380d6512e4f515dbf24cf9c5c
halil180 <halilkom180@.com>
2024-01-16 12:10:48 +0100

    fix image

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Zoom } from "react-awesome-reveal";
 import { useTranslation } from 'react-i18next';
+import backgroundImage from "../assets/space-galaxy-background.jpeg"
 
 function HelloWorld() {
 
   const {t} = useTranslation()
   
   return (
-    <div className="hero min-h-screen bg-fixed bg-center bg-no-repeat" style={{ backgroundImage: `url("https://static.vecteezy.com/system/resources/previews/003/549/556/original/space-galaxy-background-free-vector.jpg")` }}>
+    <div className="hero min-h-screen bg-fixed bg-center bg-no-repeat" style={{ backgroundImage: `url("${backgroundImage}")` }}>
   <div className="hero-overlay bg-opacity-60"></div>
   <div className="hero-content text-center text-neutral-content">
     <div className="max-w-md">
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Fade } from "react-awesome-reveal";
 import { Link, } from "react-router-dom";
 import { useTranslation } from 'react-i18next';
+import halil from "../assets/Halil_K.jpeg"
 function Hero() {
   const { t, i18n } = useTranslation();
   return (
@@ -9,7 +10,7 @@ function Hero() {
 <div className="hero min-h-screen bg-base-200">
 <Fade>
   <div className="hero-content flex-col lg:flex-row-reverse">
-    <img alt='me' src="https://media.licdn.com/dms/image/C4E03AQEaXzW5hou1tQ/profile-displayphoto-shrink_800_800/0/1638012386609?e=1677715200&v=beta&t=YITv4aLdaZfkvbXNd0oOmmu3V24rc3Bf2lpBysIcTlE" className="max-w-sm rounded-lg shadow-2xl w-full  mask mask-circle object-contain" />
+    <img alt='me' src={halil}  className="max-w-sm rounded-lg shadow-2xl w-full  mask mask-circle object-contain" />
     <div>
       <h1 className="text-5xl font-bold">{t("HiThere")}</h1>
   
